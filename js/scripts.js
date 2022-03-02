a86afe03b7d6a40a6bf17598858495970763eebb
tega <kesienametitiri024@gmail.com>
2022-03-02 14:42:00 +0100

    add jquery click effect and css hide effect to basic js & jquery concepts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,61 @@
 $(document).ready(function () {
-  $(".card-title").click(function () {
-    $(".card-text1").toggle();
-    
-    
+  $(".card-title1").click(function () {
+    $(".card-text1").toggle(); 
   });
+  $(".card-title2").click(function () {
+    $(".card-text2").toggle(); 
+  });
+  $(".card-title3").click(function () {
+    $(".card-text3").toggle(); 
+  });
+  $(".card-title4").click(function () {
+    $(".card-text4").toggle(); 
+  });
+  $(".card-title5").click(function () {
+    $(".card-text5").toggle(); 
+  });
+  $(".card-title6").click(function () {
+    $(".card-text6").toggle(); 
+  });
+  $(".card-title7").click(function () {
+    $(".card-text7").toggle(); 
+  });
+  $(".card-title8").click(function () {
+    $(".card-text8").toggle(); 
+  });
+  $(".card-title9").click(function () {
+    $(".card-text9").toggle(); 
+  });
+  $(".card-title10").click(function () {
+    $(".card-text10").toggle(); 
+  });
+  $(".card-title11").click(function () {
+    $(".card-text11").toggle(); 
+  });
+  $(".card-title12").click(function () {
+    $(".card-text12").toggle(); 
+  });
+  $(".card-title13").click(function () {
+    $(".card-text13").toggle(); 
+  });
+  $(".card-title14").click(function () {
+    $(".card-text14").toggle(); 
+  });
+  $(".card-title15").click(function () {
+    $(".card-text15").toggle(); 
+  });
+  $(".card-title16").click(function () {
+    $(".card-text16").toggle(); 
+  });
+  $(".card-title17").click(function () {
+    $(".card-text17").toggle(); 
+  });
+  $(".card-title18").click(function () {
+    $(".card-text18").toggle(); 
+  });
+ 
+  
+
 
 
 
